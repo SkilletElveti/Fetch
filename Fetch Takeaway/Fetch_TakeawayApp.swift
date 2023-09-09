2484797784a2ef923d5474874ce9c1fef3f98d22
Shubham Kamdi <shubhamvkamdi@gmail.com>
2023-09-09 10:24:52 -0700

    Initial Commit

---
+++ Fetch Takeaway/Fetch_TakeawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_TakeawayApp.swift
+//  Fetch Takeaway
+//
+//  Created by Shubham Kamdi on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_TakeawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
