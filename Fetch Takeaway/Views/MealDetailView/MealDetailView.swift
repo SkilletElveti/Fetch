5096611d056ac9df6d28ea6eda30e99ce275d24c
Shubham Kamdi <shubhamvkamdi@gmail.com>
2023-09-10 16:20:05 -0700

    - Displaying meal thumb image

--- Fetch Takeaway/Views/MealDetailView/MealDetailView.swift
+++ Fetch Takeaway/Views/MealDetailView/MealDetailView.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 import YouTubePlayerKit
+import NukeUI
 struct MealDetailView: View {
     @EnvironmentObject
     var viewModel: MealDetailViewModel
     var body: some View {
         content
+            
             .onAppear {
                 Task {
                     try await viewModel.fetchMealDetail()
@@ -22,7 +24,28 @@ struct MealDetailView: View {
     @ViewBuilder
     var content: some View {
         if let detail = viewModel.state.mealDetail {
-            Text("Data Parsed")
+            ScrollView {
+                mealImage(image: detail.strMealThumb)
+                Spacer()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func mealImage(image: String?) -> some View {
+        if let image,
+           let url = URL(string: image)
+        {
+            LazyImage(request: ImageRequest(url: url)) { state in
+                if let image = state.image {
+                   image
+                        .resizable()
+                        .frame(
+                            width: UIScreen.main.bounds.width,
+                            height: UIScreen.main.bounds.height * 0.45
+                        )
+                }
+            }
         }
     }
 }
